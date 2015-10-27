82f2879f44cfadefd85e254a25639851e48c38e3
Gaurav Malhotra <gaurav@level5websites.com>
2015-10-27 21:26:30 +0000

    Changed unit test with assistance by Maks3w

--- test/Adapter/PhpCodeTest.php
+++ test/Adapter/PhpCodeTest.php
@@ -6,12 +6,9 @@
  * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
-
 namespace ZendTest\Serializer\Adapter;
 
-use Zend\Json\Encoder;
 use Zend\Serializer;
-use ZendTest\Serializer\TestAsset\Dummy;
 
 /**
  * @covers Zend\Serializer\Adapter\PhpCode
@@ -26,78 +23,41 @@ class PhpCodeTest extends \PHPUnit_Framework_TestCase
         $this->adapter = new Serializer\Adapter\PhpCode();
     }
 
-    public function tearDown()
+    /**
+     * @dataProvider serializedValuesProvider
+     */
+    public function testSerialize($unserialized, $serialized)
     {
-        $this->adapter = null;
-    }
-
-    public function testSerializeObject()
-    {
-        $object = new Dummy();
-        $expected = Encoder::encode($object);
-
-        $data = $this->adapter->serialize($object);
-
-        $this->assertNotEmpty($data);
-        $this->assertEquals($expected, $data);
-    }
-
-    public function testUnserializeObject()
-    {
-        $expected = new Dummy();
-
-        $serialized = Encoder::encode($expected);
-        $data = $this->adapter->unserialize($serialized);
-
-        $this->assertInstanceOf(get_class($expected), $data);
+        $this->assertEquals($serialized, $this->adapter->serialize($unserialized));
     }
 
     /**
-     * @dataProvider dataProviderWithScenarios
+     * @dataProvider serializedValuesProvider
      */
-    public function testPhpCode($value, $expected)
+    public function testUnserialize($unserialized, $serialized)
     {
-        // $value serialized should be same as $expected
-        $serializeValue = $this->adapter->serialize($value);
-        $this->assertEquals($serializeValue, $expected);
-
-        // $expected unserialized should be same as $value
-        $unserializeValue = $this->adapter->unserialize($expected);
-        $this->assertEquals($unserializeValue, $value);
+        $this->assertEquals($unserialized, $this->adapter->unserialize($serialized));
     }
 
-    public function dataProviderWithScenarios()
+    public function serializedValuesProvider()
     {
         return [
-            'Serialize String' => [
-                'value' => 'test',
-                'expected' => serialize('test')
-            ],
-            'Serialize PHP Code with tags' => [
-                'value' => '<?php echo "test"; ?>',
-                'expected' => serialize('<?php echo "test"; ?>')
-            ],
-            'Serialize boolean true' => [
-                'value' => true,
-                'expected' => serialize(true)
-            ],
-            'Serialize boolean false' => [
-                'value' => false,
-                'expected' => serialize(false)
-            ],
-            'Serialize type null' => [
-                'value' => null,
-                'expected' => serialize(null)
-            ],
-            'Serialize number' => [
-                'value' => 100.12345,
-                'expected' => serialize(100.12345)
-            ],
+            // Description => [unserialized, serialized]
+            'String' => ['test', serialize('test')],
+            'true' => [true, serialize(true)],
+            'false' => [false, serialize(false)],
+            'null' => [null, serialize(null)],
+            'int' => [1, serialize(1)],
+            'float' => [1.2, serialize(1.2)],
+
             // Boolean as string
-            'Unserialize same value boolean as string' => [
-                'value' => 'false',
-                'expected' => serialize('false')
-            ],
+            '"true"' => ['true', serialize('true')],
+            '"false"' => ['false', serialize('false')],
+            '"null"' => ['null', serialize('null')],
+            '"1"' => ['1', serialize('1')],
+            '"1.2"' => ['1.2', serialize('1.2')],
+
+            'PHP Code with tags' => ['<?php echo "test"; ?>', serialize('<?php echo "test"; ?>')]
         ];
     }
 }
